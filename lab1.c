bc56baf6e6e04d9d7e7c7ac70d636f75b35e780d
ToasT1ng <jrainboww9@gmail.com>
2020-03-20 14:28:31 +0900

    add function introducing me

--- lab1.c
+++ lab1.c
@@ -9,7 +9,10 @@ int checkNum(){
 	int num = 0;
 	if (strcmp(input,"1") == 0){
 		printf("My name is Jinah Sung\n");
-	
+	} else if (strcmp(input,"2") == 0){
+		printf("I'm 24 years old. And my hobby is playing games. My favorite food is fried chicken!");
+	} else if (strcmp(input,"q") == 0 || strcmp(input,"Q") == 0){
+		printf("Quit Program\n");
 	} else {
 		num = 1;
 		printf("\nInput is not correct! Please do it again...\n\n");
